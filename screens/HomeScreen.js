fae739cd2ac876a0afb4ae0f853839ab6089d73c
Matt Simpson <matt.rc.simpson@gmail.com>
2022-11-08 14:15:38 +0200

    push new changes - ScrollView/Icons/Categories etc

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,13 @@
-import { View, Text, SafeAreaView } from 'react-native';
+import { View, Text, SafeAreaView, Image, TextInput, ScrollView } from 'react-native';
 import React, { useLayoutEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import { 
+    UserIcon, 
+    ChevronDownIcon,
+    AdjustmentsVerticalIcon,
+    MagnifyingGlassIcon
+    } from "react-native-heroicons/outline";
+import Categories from '../components/Categories';
 
 const HomeScreen = () => {
     const navigation = useNavigation();
@@ -12,8 +19,45 @@ const HomeScreen = () => {
     }, [])
 
   return (
-    <SafeAreaView>
-        <Text className="text-red-500">HomeScreen</Text>
+    <SafeAreaView className="bg-white pt-5">
+            {/* HEADER */}
+            <View className='flex-row pb-3 items-center mx-4 space-x-2 px-2'>
+                <Image
+                    source={{
+                        uri: 'https://links.papareact.com/wru'
+                    }} 
+                    className='h-7 w-7 bg-gray-300 p-4 rounded-full'
+                />
+                <View className="flex-1">
+                    <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+                    <Text className="font-bold text-xl">Current Location
+                        <ChevronDownIcon size={20} color='#00ccbb' />
+                    </Text>
+                </View>
+                <UserIcon size={35} color='#00ccbb'/>
+            </View>     
+
+            {/* SEARCH   */}
+
+            <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">            
+                <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+                    <MagnifyingGlassIcon color='gray' size={20} />
+                    <TextInput 
+                        placeholder='Restaurants and cuisines'
+                        keyboardType='default'
+                    />
+                </View>
+                <AdjustmentsVerticalIcon color='#00ccbb' />
+            </View>
+
+            {/* BODY */}
+
+            <ScrollView>
+                {/* CATEGORIES */}
+                    <Categories />
+                {/* FEATURED ROWS */}
+
+            </ScrollView>
     </SafeAreaView>
     
   )
